2fe17c2b837e3206ce98fd4b2d8a91d2bfd8f2dd
Benny Hinrichs <hinrichs.benny@gmail.com>
2018-02-12 00:21:34 -0700

    removed excess words on amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -8,7 +8,7 @@ export default ({ id, description, amount, createdAt }) => (
             <h3>{description}</h3>
         </Link>
         <p>${(amount / 100).toFixed(2)}</p>
-        <p>Created At: {moment(createdAt).format('D MMM YYYY')}</p>
+        <p>{moment(createdAt).format('D MMM YYYY')}</p>
         
     </div>
 );
